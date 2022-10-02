cebc6041fbb484a247673dc619f14e6c5ac6d07b
ianchiforov <ianchiforov@softjourn.com>
2022-10-02 19:27:45 +0300

    view for flash card

--- flashcards.py
+++ flashcards.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 
+from model import db
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
-    return render_template("welcome.html",
-                           message="Message from a view!")
+    return render_template(
+        "welcome.html",
+        message="Message from a view!"
+    )
+
+
+@app.route("/card")
+def card_view():
+    card = db[0]
+    return render_template("card.html", card=card)
